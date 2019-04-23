6ab95dd6142c78bb80a4cf08350fc96509fb313d
hakansahinsen <hakansahinsen@gmail.com>
2019-04-23 18:31:36 -0500

    fake data on about page with a puppy photo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <h1>All Photos</h1>
+    <div v-for="photo in photos">
+      <h2>{{ photo.name }}</h2>
+      <img v-bind:src="photo.url">
+      <p>Width: {{ photo.width }}</p>
+      <p>Height: {{ photo.height }}</p>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      photos: [
+        {
+          name: "doggies",
+          url:
+            "https://static.scientificamerican.com/sciam/cache/file/D059BC4A-CCF3-4495-849ABBAFAED10456_source.jpg?w=590&h=800&526ED1E1-34FF-4472-B348B8B4769AB2A1"
+        }
+      ]
+    };
+  },
+  created: function() {
+    axios.get("/api/photos").then(response => {
+      this.photos = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
