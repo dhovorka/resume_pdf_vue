a853ef329d10bbb2d9177b976f76623f24c5187e
hakansahinsen <hakansahinsen@gmail.com>
2019-04-22 19:13:22 -0500

    hakan.edit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App This is Sandy. Screw Dan." />
+    <HelloWorld msg="Welcome to Your Vue.js App This is Sandy. Screw Dan. Killing it" />
   </div>
 </template>
 
