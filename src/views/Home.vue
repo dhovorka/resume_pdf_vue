11a1fa1b4cfe8137dba1979ef673b4d6282e262e
“Sandy <sandymai0108@gmail.com>
2019-04-22 20:59:33 -0500

    add button to print save pdf and print text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,40 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App This is Sandy. Screw Dan." />
+    <p>{{ message }}</p>
+
+    <div>
+      <button v-on:click="printresume()">Click to Print PDF</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+var axios = require("axios");
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      message: "Hello"
+    };
+  },
+  methods: {
+    printresume: function() {
+      var pdf = new jsPDF();
+      pdf.canvas.height = 72 * 11;
+      pdf.canvas.width = 72 * 8.5;
+
+      pdf.fromHTML(document.body);
+
+      pdf.text("WE DID IT!", 10, 10);
+      pdf.save("test.pdf");
+    }
   }
 };
 </script>
