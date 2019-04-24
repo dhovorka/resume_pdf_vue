8c0d3bb34a786bbe97414a543b39c6878f116838
Daniel Hovorka <dhovorka10@gmail.com>
2019-04-23 20:46:51 -0500

    linked frontend to backend via heroku

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="container">
-    <button v-on:click="printresume()">Here is your resume</button>
-
     <h1>All Resumes</h1>
-    <div v-for="resume in resumes">
-      <h2>{{ resume.name }}</h2>
-      <h2>{{ resume.title }}</h2>
-      <h2>{{ resume.department }}</h2>
+    <div v-for="student in students">
+      <p>First Name: {{ student.first_name }}</p>
+      <p>Last Name: {{ student.last_name }}</p>
+      <p>Email: {{ student.email }}</p>
+      <p>Phone: {{ student.phone_number }}</p>
+      <p>Bio: {{ student.short_bio }}</p>
+      <p>Linkedin: {{ student.linkedin_url }}</p>
+      <p>Twitter: {{ student.twiter_handle }}</p>
+      <p>Website: {{ student.personal_website_url }}</p>
+      <p>Github: {{ student.github_url }}</p>
+      <p>Online: {{ student.online_resume_url }}</p>
+      <p>Photo: {{ student.photo_url }}</p>
+      <button v-on:click="printresume()">Here is your resume</button>
     </div>
   </div>
 </template>
-
 
 <script>
 import axios from "axios";
@@ -18,18 +24,12 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      resumes: [
-        {
-          name: "Jordan",
-          title: "Junior Buyer",
-          department: "Retail"
-        }
-      ]
+      students: []
     };
   },
   created: function() {
-    axios.get("/api/resumes").then(response => {
-      this.resumes = response.data;
+    axios.get("https://salty-gorge-38704.herokuapp.com/api/students/").then(response => {
+      this.students = response.data;
     });
   },
   methods: {
